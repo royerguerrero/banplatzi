1d624607f10bf95d3b2a85632046e4903ab9080d
Royer Guerrero <royjuni3431@gmail.com>
2020-01-20 07:11:19 -0500

    Añadiendo el 'balance' de los clientes y sus funciones deposit y withdraw

--- clients.py
+++ clients.py
@@ -1,29 +1,31 @@
 class Client:
 
-    def __init__(self, name, identification, account_number, number_phone, email):
+    def __init__(self, name, identification, account_number, number_phone, email, balance):
         self.name = name
         self.identification = identification
         self.account_number = account_number
         self.number_phone = number_phone
         self.email = email
+        self.balance = balance
 
 class ClientBook:
 
     def __init__(self):
         self._clients = []
 
-    def add(self, name, identification, account_number, number_phone, email):
-        client = Client(name, identification, account_number, number_phone, email)
+    def add(self, name, identification, account_number, number_phone, email, balance = 0):
+        client = Client(name, identification, account_number, number_phone, email, balance)
         self._clients.append(client)
 
     def show_all(self):
         for client in self._clients:
-            print(f'Account Number: {client.account_number} >> Client Name: {client.name} | Client Identificacion: {client.identification} | Client Number Phone: {client.number_phone} | Client Email: {client.email}')
+            print(f'Account Number: {client.account_number} >> Client Name: {client.name} | Client Identificacion: {client.identification} | Client Balance: {client.balance} | Client Number Phone: {client.number_phone} | Client Email: {client.email}')
 
 
     def _print_client(self, client):
             print(' --- ' * 10)
             print(f'Account Number: {client.account_number} >> Client Name: {client.name} | Client Identificacion: {client.identification} | Client Number Phone: {client.number_phone} | Client Email: {client.email}')
+            print(' --- ' * 10)
 
 
     def search(self, account_number):
@@ -40,16 +42,28 @@ class ClientBook:
         client = self.search(account_number)
         self._clients.remove(client)
 
-    # TODO: La funcion no esta remplazando esta añadiendo el cliente???
-    def update(self, account_number, updated_name, updated_identification, updated_account_number, updated_number_phone, updated_email):
-        client = self.search(account_number)
+    
+    def update(self, client, name, identification, account_number, number_phone, email):
 
-        client.name = updated_name
-        client.identification = updated_identification
-        client.account_number = updated_account_number
-        client.number_phone = updated_number_phone
-        client.email = updated_email
+        client.name = name
+        client.identification = identification
+        client.account_number = account_number
+        client.number_phone = number_phone
+        client.email = email
+    
 
+    def deposit(self, client, value):
+        client.balance += int(value)
+
+    def withdraw(self, client, value):
+        
+        withdraw = client.balance - int(value)
+        
+        if withdraw >= 0:
+            client.balance = withdraw
+        else:
+            print('Underfunding')
+        
 
 
 book = ClientBook()
@@ -61,7 +75,10 @@ book.add('Andres', '025', '777', '854278945', 'jfdask@jidfkal')
 
 book.show_all()
 
+client = book.search('000')
 
-book.update('000', 'Peter', '132', '123', '789', 'peter@mail.com')
+book.deposit(client, 1000)
+
+book.withdraw(client, 100)
 
 book.show_all()
